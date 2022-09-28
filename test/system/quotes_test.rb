b2ed397f94929616d9c8afb7bfa60aba3726ddb5
pauline rouger <paulinerouge@hotmail.com>
2022-09-28 16:18:31 +0800

    system_test quotes added

---
+++ test/system/quotes_test.rb
@@ -0,0 +1,31 @@
+require "application_system_test_case"
+
+class QuotesTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit quotes_url
+  #
+  #   assert_selector "h1", text: "Quotes"
+  # end
+
+  test "Creating a new quote" do
+    # When we visit the Quotes#index page
+    # we expect to see a title with the text "Quotes"
+    visit quotes_path
+    assert_selector "h1", text: "Quotes"
+
+    # When we click on the link with the text "New quote"
+    # we expect to land on a page with the title "New quote"
+    click_on "New quote"
+    assert_selector "h1", text: "New quote"
+
+    # When we fill in the name input with "Capybara quote"
+    # and we click on "Create Quote"
+    fill_in "Name", with: "Capybara quote"
+    click_on "Create quote"
+
+    # We expect to be back on the page with the title "Quotes"
+    # and to see our "Capybara quote" added to the list
+    assert_selector "h1", text: "Quotes"
+    assert_text "Capybara quote"
+  end
+end
