1cdc79a988a8767922f2b436e5eed4222e54820a
pauline rouger <paulinerouge@hotmail.com>
2022-10-03 19:57:40 +0800

    Users, companies and, quotes associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_03_114844) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_03_115429) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_03_114844) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "company_id", null: false
+    t.index ["company_id"], name: "index_quotes_on_company_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,8 +36,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_03_114844) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "company_id", null: false
+    t.index ["company_id"], name: "index_users_on_company_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "quotes", "companies"
+  add_foreign_key "users", "companies"
 end
