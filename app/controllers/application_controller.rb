bc7083dd29f7bdf6a0485b75d7a2e2efb0a8578c
pauline rouger <paulinerouge@hotmail.com>
2022-10-03 20:57:51 +0800

    Added current_company helper with helper_method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,12 @@ class ApplicationController < ActionController::Base
   # and remove it when it has the expected styles
   # before_action -> { sleep 3 }
   before_action :authenticate_user!, unless: :devise_controller?
+
+  private
+
+  def current_company
+    @current_company ||= current_user.company if user_signed_in?
+  end
+  helper_method :current_company
+  
 end
