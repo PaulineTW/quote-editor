2076a5539ae007632130302ba68c37bb8cbc91fa
pauline rouger <paulinerouge@hotmail.com>
2022-10-03 16:56:44 +0800

    Fixing test Showing a quote dupe

--- test/system/quotes_test.rb
+++ test/system/quotes_test.rb
@@ -7,7 +7,7 @@ class QuotesTest < ApplicationSystemTestCase
   #   assert_selector "h1", text: "Quotes"
   # end
   setup do
-    @quote = quotes(:first) # Reference to the first fixture quote
+    @quote = Quote.ordered.first # Reference to the first fixture quote
   end
 
   test "Showing a quote" do
@@ -23,27 +23,18 @@ class QuotesTest < ApplicationSystemTestCase
     visit quotes_path
     assert_selector "h1", text: "Quotes"
 
-    # When we click on the link with the text "New quote"
-    # we expect to land on a page with the title "New quote"
-    click_on "New quote"
-    assert_selector "h1", text: "New quote"
-
     # When we fill in the name input with "Capybara quote"
-    # and we click on "Create Quote"
+    # and we click on "New quote"
+    click_on "New quote"
     fill_in "Name", with: "Capybara quote"
-    click_on "Create quote"
 
     # We expect to be back on the page with the title "Quotes"
     # and to see our "Capybara quote" added to the list
     assert_selector "h1", text: "Quotes"
+    click_on "Create quote"
+
+    assert_selector "h1", text: "Quotes"
     assert_text "Capybara quote"
-  end
-
-  test "Showing a quote" do
-    visit quotes_path
-    click_link @quote.name
-
-    assert_selector "h1", text: @quote.name
   end
 
   test "Updating a quote" do
@@ -51,9 +42,9 @@ class QuotesTest < ApplicationSystemTestCase
     assert_selector "h1", text: "Quotes"
 
     click_on "Edit", match: :first
-    assert_selector "h1", text: "Edit quote"
-
     fill_in "Name", with: "Updated quote"
+
+    assert_selector "h1", text: "Quotes"
     click_on "Update quote"
 
     assert_selector "h1", text: "Quotes"
