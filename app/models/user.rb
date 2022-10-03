b597781c7e1bec204ab4e5b1b40f5ec2e011fb87
pauline rouger <paulinerouge@hotmail.com>
2022-10-03 19:52:32 +0800

    Added fixtures for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  # :registerable, :recoverable, :rememberable
+  devise :database_authenticatable, :validatable
 end
