b95087130e67e0c21fac9145226c684f2d66f48f
pauline rouger <paulinerouge@hotmail.com>
2022-10-03 19:47:40 +0800

    Adding companies

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_28_082315) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_03_114330) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "companies", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "quotes", force: :cascade do |t|
     t.string "name", null: false
