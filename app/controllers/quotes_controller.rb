7a750409920c1923b45403e2ce408a419310dc88
pauline rouger <paulinerouge@hotmail.com>
2022-10-03 15:51:31 +0800

    Added Turbo Stream views for create

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -16,7 +16,10 @@ class QuotesController < ApplicationController
     @quote = Quote.new(quote_params)
 
     if @quote.save
-      redirect_to quotes_path, notice: "Quote was successfully created."
+      respond_to do |format|
+        format.html { redirect_to quotes_path, notice: "Quote was successfully created." }
+        format.turbo_stream
+      end
     else
       render :new, status: :unprocessable_entity
     end
