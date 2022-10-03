65854a959bd4f83bc9b4da54a03593e6ff3117dc
pauline rouger <paulinerouge@hotmail.com>
2022-10-03 20:41:57 +0800

    PagesController added + routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,5 @@ class ApplicationController < ActionController::Base
     # Add this line to see the progress bar long enough
   # and remove it when it has the expected styles
   # before_action -> { sleep 3 }
+  before_action :authenticate_user!, unless: :devise_controller?
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  root to: "pages#home"
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  
+
   resources :quotes
 
 end
