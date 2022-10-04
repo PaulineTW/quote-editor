5561f171cfd2e716c79bc77f4c5003f27d1346ea
pauline rouger <paulinerouge@hotmail.com>
2022-10-04 17:11:33 +0800

    Refactoring the error notification message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,15 @@
 module ApplicationHelper
+  #the application has only two helpers, so it's fine to keep those helpers in the ApplicationHelper
   def render_turbo_stream_flash_messages
     turbo_stream.prepend "flash", partial: "layouts/flash"
   end
+
+  def form_error_notification(object)
+    if object.errors.any?
+      tag.div class: "error-message" do
+        object.errors.full_messages.to_sentence.capitalize
+      end
+    end
+  end
+
 end
