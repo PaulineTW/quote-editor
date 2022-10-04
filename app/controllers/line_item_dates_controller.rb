031077f196771c074f729d829a04327ae7aced17
pauline rouger <paulinerouge@hotmail.com>
2022-10-04 19:00:48 +0800

    Updating line item dates with Turbo

--- app/controllers/line_item_dates_controller.rb
+++ app/controllers/line_item_dates_controller.rb
@@ -24,7 +24,10 @@ class LineItemDatesController < ApplicationController
 
   def update
     if @line_item_date.update(line_item_date_params)
-      redirect_to quote_path(@quote), notice: "Date was successfully updated."
+      respond_to do |format|
+        format.html { redirect_to quote_path(@quote), notice: "Date was successfully updated." }
+        format.turbo_stream { flash.now[:notice] = "Date was successfully updated." }
+      end
     else
       render :edit, status: :unprocessable_entity
     end
