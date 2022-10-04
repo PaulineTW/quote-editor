84b2220a98059c99f9ba681d1886028c6c767b1f
pauline rouger <paulinerouge@hotmail.com>
2022-10-04 18:45:40 +0800

    Implemented test for previous_date method

--- test/models/line_item_date_test.rb
+++ test/models/line_item_date_test.rb
@@ -4,4 +4,12 @@ class LineItemDateTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "#previous_date returns the quote's previous date when it exist" do
+    assert_equal line_item_dates(:today), line_item_dates(:next_week).previous_date
+  end
+
+  test "#previous_date returns nil when the quote has no previous date" do
+    assert_nil line_item_dates(:today).previous_date
+  end
 end
