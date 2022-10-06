cf241401ce7a0d5e0b1d66218cf8e7ab192b2504
pauline rouger <paulinerouge@hotmail.com>
2022-10-06 15:22:34 +0800

    Change update action line items with Turbo

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -25,7 +25,10 @@ class LineItemsController < ApplicationController
 
   def update
     if @line_item.update(line_item_params)
-      redirect_to quote_path(@quote), notice: "Item was successfully updated."
+      respond_to do |format|
+        format.html { redirect_to quote_path(@quote), notice: "Item was successfully updated." }
+        format.turbo_stream { flash.now[:notice] = "Item was successfully updated." }
+      end
     else
       render :edit, status: :unprocessable_entity
     end
