eb3623fc460b304e03f0c1b6e093af7d65346e44
pauline rouger <paulinerouge@hotmail.com>
2022-10-06 15:24:26 +0800

    Change Destroying line items with Turbo

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -37,7 +37,10 @@ class LineItemsController < ApplicationController
   def destroy
     @line_item.destroy
 
-    redirect_to quote_path(@quote), notice: "Item was successfully deleted."
+    respond_to do |format|
+      format.html { redirect_to quote_path(@quote), notice: "Date was successfully destroyed." }
+      format.turbo_stream { flash.now[:notice] = "Date was successfully destroyed." }
+    end
   end
 
   private
