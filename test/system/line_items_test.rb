1d4b074af0117b5bab5ea5c9fd7989163eafc0f6
pauline rouger <paulinerouge@hotmail.com>
2022-10-06 16:59:30 +0800

    Test added for total amount is correctly updated right after create, update, or delete a line item

--- test/system/line_items_test.rb
+++ test/system/line_items_test.rb
@@ -29,6 +29,7 @@ class LineItemSystemTest < ApplicationSystemTestCase
     assert_selector "h1", text: "First quote"
     assert_text "Animation"
     assert_text number_to_currency(1234)
+    assert_text number_to_currency(@quote.total_price)
   end
 
   test "Updating a line item" do
@@ -45,6 +46,7 @@ class LineItemSystemTest < ApplicationSystemTestCase
 
     assert_text "Capybara article"
     assert_text number_to_currency(1234)
+    assert_text number_to_currency(@quote.total_price)
   end
 
   test "Destroying a line item" do
@@ -59,5 +61,6 @@ class LineItemSystemTest < ApplicationSystemTestCase
     within "##{dom_id(@line_item_date)}" do
       assert_no_text @line_item.name
     end
+    assert_text number_to_currency(@quote.total_price)
   end
 end
